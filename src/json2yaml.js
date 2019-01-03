c2cd20c243c9840a1b7fa96f6b8074940a66fea1
Dawid Malinowski <dawidmalina@gmail.com>
2019-01-03 09:35:31 +0100

    Makes https://www.jslint.com/ happy

--- src/json2yaml.js
+++ src/json2yaml.js
@@ -1,96 +1,123 @@
+/*jslint for:true */
 var spacing = "  ";
 
 function getType(obj) {
-  var type = typeof obj;
-  if (obj instanceof Array) {
-    return 'array';
-  } else if (type == 'string') {
-    return 'string';
-  } else if (type == 'boolean') {
-    return 'boolean';
-  } else if (type == 'number') {
-    return 'number';
-  } else if (type == 'undefined' || obj === null) {
-    return 'null';
-  } else {
-    return 'hash';
-  }
-}
-
-function convert(obj, ret) {
-  var type = getType(obj);
-
-  switch(type) {
-    case 'array':
-      convertArray(obj, ret);
-      break;
-    case 'hash':
-      convertHash(obj, ret);
-      break;
-    case 'string':
-      convertString(obj, ret);
-      break;
-    case 'null':
-      ret.push('null');
-      break;
-    case 'number':
-      ret.push(obj.toString());
-      break;
-    case 'boolean':
-      ret.push(obj ? 'true' : 'false');
-      break;
-  }
-}
-
-function convertArray(obj, ret) {
-  for (var i=0; i<obj.length; i++) {
-    var ele     = obj[i];
-    var recurse = [];
-    convert(ele, recurse);
-
-    for (var j=0; j<recurse.length; j++) {
-      ret.push((j == 0 ? "- " : spacing) + recurse[j]);
+    "use strict";
+    var type = typeof obj;
+    if (Array.isArray(obj)) {
+        return "array";
+    } else if (type === "string") {
+        return "string";
+    } else if (type === "boolean") {
+        return "boolean";
+    } else if (type === "number") {
+        return "number";
+    } else if (type === "undefined" || obj === null) {
+        return "null";
+    } else {
+        return "hash";
     }
-  }
-}
-
-function convertHash(obj, ret) {
-  for (var k in obj) {
-    var recurse = [];
-    if (obj.hasOwnProperty(k)) {
-      var ele = obj[k];
-      convert(ele, recurse);
-      var type = getType(ele);
-      if (type == 'string' || type == 'null' || type == 'number' || type == 'boolean') {
-        ret.push(normalizeString(k) + ': ' +  recurse[0]);
-      } else {
-        ret.push(normalizeString(k) + ': ');
-        for (var i=0; i<recurse.length; i++) {
-          ret.push(spacing + recurse[i]);
-        }
-      }
-    }
-  }
 }
 
 function normalizeString(str) {
-  if (str.match(/^[\w]+$/)) {
-    return str;
-  } else {
-    return '"'+escape(str).replace(/%u/g,'\\u').replace(/%U/g,'\\U').replace(/%/g,'\\x')+'"';
-  }
+    "use strict";
+    if (str.match(/^[\w]+$/)) {
+        return str;
+    } else {
+        return "\"" + decodeURI(str).replace(/%u/g, "\\u").replace(/%U/g, "\\U").replace(/%/g, "\\x") + "\"";
+    }
 }
 
 function convertString(obj, ret) {
-  ret.push(normalizeString(obj));
+    "use strict";
+    ret.push(normalizeString(obj));
 }
 
-var json2yaml = function(obj) {
-  if (typeof obj == 'string') {
-    obj = JSON.parse(obj);
-  }
+function convertArray(obj, ret) {
+    "use strict";
+    var i;
+    var j;
+    var ele;
+    var recurse;
+    for (i = 0; i < obj.length; i += 1) {
+        ele = obj[i];
+        recurse = [];
+        convert(ele, recurse);
+        for (j = 0; j < recurse.length; j += 1) {
+            if (j === 0) {
+                ret.push("- " + recurse[j]);
+            } else {
+                ret.push(spacing + recurse[j]);
+            }
+        }
+    }
+}
 
-  var ret = [];
-  convert(obj, ret);
-  return ret.join("\n");
+function convertHash(obj, ret) {
+    "use strict";
+    var k;
+    var i;
+    var recurse;
+    var ele;
+    var type;
+    for (k = 0; k < obj.length; k += 1) {
+        recurse = [];
+        if (obj.hasOwnProperty(k)) {
+            ele = obj[k];
+            convert(ele, recurse);
+            type = getType(ele);
+            if (type === "string" ||
+                    type === "null" ||
+                    type === "number" ||
+                    type === "boolean") {
+                ret.push(normalizeString(k) + ": " + recurse[0]);
+            } else {
+                ret.push(normalizeString(k) + ": ");
+                for (i = 0; i < recurse.length; i += 1) {
+                    ret.push(spacing + recurse[i]);
+                }
+            }
+        }
+    }
+}
+
+function convert(obj, ret) {
+    "use strict";
+    var type = getType(obj);
+
+    switch (type) {
+    case "array":
+        convertArray(obj, ret);
+        break;
+    case "hash":
+        convertHash(obj, ret);
+        break;
+    case "string":
+        convertString(obj, ret);
+        break;
+    case "null":
+        ret.push("null");
+        break;
+    case "number":
+        ret.push(obj.toString());
+        break;
+    case "boolean":
+        if (obj) {
+            ret.push("true");
+        } else {
+            ret.push("false");
+        }
+        break;
+    }
+}
+
+var json2yaml = function (obj) {
+    "use strict";
+    if (getType(obj) === "string") {
+        obj = JSON.parse(obj);
+    }
+
+    var ret = [];
+    convert(obj, ret);
+    return ret.join("\n");
 };
